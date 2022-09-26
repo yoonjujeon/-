9cc0ed8260d498e37844c6c75b1e49ad4b02aeaa
yoonjujeon <105338895+yoonjujeon@users.noreply.github.com>
2022-09-26 16:22:42 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,34 @@
+#define PIN_LED 7
+
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED,0);
+  delay(1000);
+  digitalWrite(PIN_LED,1);
+  delay(100);
+  digitalWrite(PIN_LED,0);
+  delay(100);
+  digitalWrite(PIN_LED,1);
+  delay(100);
+  digitalWrite(PIN_LED,0);
+  delay(100);
+  digitalWrite(PIN_LED,1);
+  delay(100);
+  digitalWrite(PIN_LED,0);
+  delay(100);
+  digitalWrite(PIN_LED,1);
+  delay(100);
+  digitalWrite(PIN_LED,0);
+  delay(100);
+  digitalWrite(PIN_LED,1);
+  delay(100);
+  digitalWrite(PIN_LED,0);
+  delay(100);
+}
+
+void loop() {
+  while(1){
+    digitalWrite(PIN_LED,1);
+  }
+}
